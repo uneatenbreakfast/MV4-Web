ec9048d951a9afc684bde56ca45b5b5c048eb5ca
uneatenbreakfast <uneatenbreakfast@gmail.com>
2014-09-22 23:18:29 +1200

    add all

---
+++ Assignment2/Controllers/HomeController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Assignment2.Models;
+
+namespace Assignment2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "M";
+
+            using (EmployeesContext db = new EmployeesContext())
+            {
+               /* int j = db.Employees.Count();
+                ViewBag.Message = String.Format("We have {0} records.", j);
+                var data = db.Employees.ToList();
+                return View(data);*/
+
+                var cabincrew = 
+                    from e in db.crew 
+                    from p in db.personDetails
+                    from f in db.flight
+                    from a in db.aircraftDetails
+                    from at in db.airType
+                    from r in db.routeDetails
+                    from ap in db.airportDetails
+                    from ap2 in db.airportDetails
+                    where 
+                    e.cabinCrewId == p.id &&
+                    f.id == e.flightId &&
+                    f.aircraft == a.id &&
+                    a.aircraftType == at.id &&
+                    f.route == r.id &&
+                    r.fromAirport == ap.id &&
+                    r.toAirport == ap2.id
+                    
+                   // orderby e.Surname 
+                    select new crewGrid { 
+                        name = p.name,
+                        flightDay = f.flightDay,
+                        aircraftModel = at.model,
+                        fromAirport = ap.code,
+                        toAirport = ap2.code,
+                        startDate = e.startDate
+
+                    };
+
+               cabincrew.ToList();
+
+               return View(cabincrew.ToList());
+
+            }
+           return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult newCabinAssignment()
+        {
+            ViewBag.Message = "Your contact page.x";
+
+            Employee e = new Employee();
+            return View(e);
+        }
+    }
+}
---
+++ Assignment2/Models/Employee.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema; 
+
+namespace Assignment2.Models
+{
+    
+    public class EmployeesContext : DbContext{
+        
+        public EmployeesContext() : base("MySqlConnection"){}
+        public DbSet<Employee> crew { get; set; } 
+        public DbSet<Person> personDetails { get; set; }
+        public DbSet<Flight> flight { get; set; }
+        public DbSet<Aircraft> aircraftDetails {get;set;}
+        public DbSet<AircraftType> airType { get; set; }
+        public DbSet<Route> routeDetails { get; set; }
+        public DbSet<Airport> airportDetails { get; set; }
+    }
+
+
+    /*
+     *  Person name 
+     *  The flight day (i.e. “Monday” or such) 
+     *  The “From” airport for the route (show the 3-digit code, e.g. “AKL”) 
+     *  The “To” airport for the route (also as 3-digit code)
+     *  The aircraft model for that route (e.g. “A380”) 
+     *  The start date of that person’s assignment to crew that route in yyyy-mm-dd format 
+     * 
+     * 
+     * 
+     * */
+
+
+    public class crewGrid { 
+        public string name { get; set; } 
+        public string flightDay { get; set; } 
+        public string fromAirport { get; set; } 
+        public string toAirport { get; set; } 
+        public string aircraftModel { get; set; }
+        public DateTime startDate { get; set; }
+    }
+
+    [Table("Aircraft")]
+    public class Aircraft
+    {
+        [Key]
+        public int id { get; set; }
+        public int aircraftType { get; set; }
+    }
+     [Table("AircraftType")]
+    public class AircraftType
+    {
+        [Key]
+        public int id { get; set; }
+        public string model { get; set; }
+    }
+
+    [Table("Route")]
+     public class Route
+     {
+        [Key]
+        public int id { get; set; }
+        public int fromAirport{get;set;}
+        public int toAirport{get;set;}
+        public int distance{get;set;}
+     }
+
+    [Table("Airport")]
+    public class Airport
+    {
+        [Key]
+        public int id { get; set; }
+        public int city { get; set; }
+        public string code { get; set; }
+    }
+
+    
+    [Table("flight")]
+    public class Flight
+    {
+        [Key]
+        public int id { get; set; }
+        public int route { get; set; }
+        public string flightDay { get; set; }
+        public int aircraft { get; set; }
+    }
+
+    [Table("Person")]
+    public class Person
+    {
+        [Key]
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+
+
+
+
+
+    [Table("FlightCabinCrew")]
+    public class Employee
+    {
+        private DateTime d;
+
+        [Key]
+        public int cabinCrewId { get; set; }
+        public int flightId { get; set; }
+
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [DateAttributeLow]
+        public DateTime startDate
+        {
+
+            get { return (d.Day > 1 ? d : DateTime.Now); }
+            set { d = value; }
+        }
+    }
+
+
+
+  
+
+    public class DateAttributeLow : RangeAttribute { 
+        public DateAttributeLow() : base(typeof(DateTime),
+            DateTime.Now.ToShortDateString(),
+            DateTime.MaxValue.ToShortDateString()) { 
+            this.ErrorMessage = "Start date cannot be earlier than today"; 
+        } 
+    }
+}
